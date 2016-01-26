34d73175a07baee94c1329d200dbb6597afa1ecb
xiner <xinerworld@qq.com>
2016-01-26 20:39:27 +0800

    first init

---
+++ src/com/xiner/dcball/MainActivity.java
@@ -0,0 +1,31 @@
+package com.xiner.dcball;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	protected void onPause()
+	{
+		super.onPause();
+	}
+	
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
+	}
+	
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+	}
+}
